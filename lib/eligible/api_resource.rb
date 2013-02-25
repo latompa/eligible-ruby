06177a9d6cc316d0746f60359fc17171e93cbe73
Janet Chang <janetlaichang@gmail.com>
2013-02-25 10:13:06 -0800

    DRY up api-resource subclasses by moving self.get method to api-resource

--- lib/eligible/api_resource.rb
+++ lib/eligible/api_resource.rb
@@ -10,5 +10,10 @@ module Eligible
       end
       "/#{CGI.escape(class_name.downcase)}/all.json"
     end
+
+    def self.get(params, api_key=nil)
+      response, api_key = Eligible.request(:get, url, api_key, params)
+      Util.convert_to_eligible_object(response, api_key)
+    end
   end
 end
--- lib/eligible/claim.rb
+++ lib/eligible/claim.rb
@@ -1,10 +1,5 @@
 module Eligible
   class Claim < APIResource
-    def self.get(params, api_key=nil)
-      response, api_key = Eligible.request(:get, url, api_key, params)
-      Util.convert_to_eligible_object(response, api_key)
-    end
-
     def status
       error ? nil : to_hash
     end
