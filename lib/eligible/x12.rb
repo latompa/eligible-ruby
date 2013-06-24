cb55bca6eedfc9f7dab8e57ed9b6152769e3fd31
Ivan Muller <mullerivan@gmail.com>
2013-06-24 01:28:17 -0300

    add test param in x12

--- lib/eligible/x12.rb
+++ lib/eligible/x12.rb
@@ -6,7 +6,8 @@ module Eligible
       require 'net/https'
       require 'uri'
       uri = URI.parse("https://gds.eligibleapi.com")
-      post_args = "x12=#{params}&api_key=#{Eligible.api_key}"
+
+      post_args = "x12=#{params}&api_key=#{Eligible.api_key}&test=#{Eligible.test}"
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
