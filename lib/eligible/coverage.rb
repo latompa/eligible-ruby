69021661048d33e3f8bbb14a72a166ac96dbb738
Ivan Müller <mulleivan@gmail.com>
2013-08-15 11:31:17 +1200

    add documentation from Batch post Demographic

--- lib/eligible/coverage.rb
+++ lib/eligible/coverage.rb
@@ -22,6 +22,8 @@ module Eligible
     def self.batch_medicare_post(params, api_key=nil)
       response, api_key = Eligible.request(:post, '/medicare/coverage/batch.json', api_key, params)
       Util.convert_to_eligible_object(response, api_key)
+    end
+
   end
 
 end
