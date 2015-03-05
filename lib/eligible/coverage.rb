2bed34cb2dd01bb19581a54104242097bc1512ae
SimonKaluza <kaluza.simon@gmail.com>
2015-03-05 11:35:57 -0500

    Extended Eligible::Coverage class to support the recent cost_estimate endpoint added to EligibleAPI

--- lib/eligible/coverage.rb
+++ lib/eligible/coverage.rb
@@ -8,6 +8,11 @@ module Eligible
         Util.convert_to_eligible_object(response, api_key)
       end
 
+      def cost_estimate(params, api_key=nil)
+        response, api_key = Eligible.request(:get, '/coverage/cost_estimate.json', api_key, params)
+        Util.convert_to_eligible_object(response, api_key)
+      end
+
       def batch_post(params, api_key=nil)
         response, api_key = Eligible.request(:post, '/coverage/all/batch.json', api_key, params)
         Util.convert_to_eligible_object(response, api_key)
