9e548f5c7c04668f1aaf563ae2adb488256f14a6
Aaron Bedra <aaron@aaronbedra.com>
2016-01-05 14:07:37 -0500

    Update spec for valid URI

--- spec/lib/eligible_spec.rb
+++ spec/lib/eligible_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Eligible do
   it 'fails when certificate presented does not match embedded fingerprint' do
-    Eligible.api_key = "doesn't matter"
+    Eligible.api_key = "foo"
     allow(Eligible).to receive(:valid_fingerprint?).and_return(false)
     expect { Eligible::Coverage.get({}) }.to raise_error(Eligible::APIConnectionError)
   end
