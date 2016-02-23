6b9faa542ef87a676c5134f01bf134ab9654da1b
Oleksiy Kovyrin <alexey@kovyrin.net>
2016-02-23 13:24:00 -0500

    API key argument is not required

--- lib/eligible/coverage_resource.rb
+++ lib/eligible/coverage_resource.rb
@@ -1,10 +1,10 @@
 module Eligible
   class CoverageResource < APIResource
-    def self.get(params, api_key)
+    def self.get(params, api_key = nil)
       send_request(:get, get_uri, api_key, params)
     end
 
-    def self.batch_post(params, api_key)
+    def self.batch_post(params, api_key = nil)
       send_request(:post, post_uri, api_key, params)
     end
 
