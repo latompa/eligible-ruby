1c5d87a9a8bfc0cd0c558389c7dbb0bf886f4009
Ankit Dimania <ankitdimania@eligible.com>
2016-07-26 18:48:42 +0530

    Add batch payment status

--- lib/eligible/payment.rb
+++ lib/eligible/payment.rb
@@ -3,5 +3,9 @@ module Eligible
     def self.get(params, api_key = nil)
       send_request(:get, '/payment/status.json', api_key, params)
     end
+
+    def self.batch(params, api_key = nil)
+      send_request(:get, '/batch/payment/status.json', api_key, params)
+    end
   end
 end
