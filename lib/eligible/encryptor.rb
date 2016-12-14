4f28abea9720a6601466ee13529f12beab80f9bc
Aaron Bedra <aaron@aaronbedra.com>
2016-12-14 09:17:28 -0600

    Fix indentation

--- lib/eligible/encryptor.rb
+++ lib/eligible/encryptor.rb
@@ -3,9 +3,7 @@ require 'openssl'
 module Eligible
   # A simple wrapper for the standard OpenSSL library
   module Encryptor
-
     extend self
-
     # The default options to use when calling the <tt>encrypt</tt> and <tt>decrypt</tt> methods
     #
     # Defaults to { algorithm: 'aes-256-gcm',
@@ -16,11 +14,13 @@ module Eligible
     #
     # Run 'openssl list-cipher-commands' in your terminal to view a list all cipher algorithms that are supported on your platform
     def default_options
-      @default_options ||= { algorithm: 'aes-256-cbc',
-                             auth_data: '',
-                             insecure_mode: false,
-                             hmac_iterations: 2000,
-                             v2_gcm_iv: false }
+      @default_options ||= {
+        algorithm: 'aes-256-cbc',
+        auth_data: '',
+        insecure_mode: false,
+        hmac_iterations: 2000,
+        v2_gcm_iv: false
+      }
     end
 
     # Encrypts a <tt>:value</tt> with a specified <tt>:key</tt> and <tt>:iv</tt>.
@@ -51,65 +51,71 @@ module Eligible
 
     protected
 
-      def crypt(cipher_method, *args) #:nodoc:
-        options = default_options.merge(value: args.first).merge(args.last.is_a?(Hash) ? args.last : {})
-        raise ArgumentError.new('must specify a key') if options[:key].to_s.empty?
-        cipher = OpenSSL::Cipher.new(options[:algorithm])
-        cipher.send(cipher_method)
-        unless options[:insecure_mode]
-          raise ArgumentError.new("key must be #{cipher.key_len} bytes or longer") if options[:key].bytesize < cipher.key_len
-          raise ArgumentError.new('must specify an iv') if options[:iv].to_s.empty?
-          raise ArgumentError.new("iv must be #{cipher.iv_len} bytes or longer") if options[:iv].bytesize < cipher.iv_len
-        end
-        if options[:iv]
-          # This is here for backwards compatibility for Encryptor v2.0.0.
-          cipher.iv = options[:iv] if options[:v2_gcm_iv]
-          if options[:salt].nil?
-            # Use a non-salted cipher.
-            # This behaviour is retained for backwards compatibility. This mode
-            # is not secure and new deployments should use the :salt options
-            # wherever possible.
-            cipher.key = options[:key]
-          else
-            # Use an explicit salt (which can be persisted into a database on a
-            # per-column basis, for example). This is the preferred (and more
-            # secure) mode of operation.
-            cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(options[:key], options[:salt], options[:hmac_iterations], cipher.key_len)
-          end
-          cipher.iv = options[:iv] unless options[:v2_gcm_iv]
+    def crypt(cipher_method, *args) #:nodoc:
+      options = default_options.merge(value: args.first).merge(args.last.is_a?(Hash) ? args.last : {})
+      raise ArgumentError.new('must specify a key') if options[:key].to_s.empty?
+      cipher = OpenSSL::Cipher.new(options[:algorithm])
+      cipher.send(cipher_method)
+
+      unless options[:insecure_mode]
+        raise ArgumentError.new("key must be #{cipher.key_len} bytes or longer") if options[:key].bytesize < cipher.key_len
+        raise ArgumentError.new('must specify an iv') if options[:iv].to_s.empty?
+        raise ArgumentError.new("iv must be #{cipher.iv_len} bytes or longer") if options[:iv].bytesize < cipher.iv_len
+      end
+
+      if options[:iv]
+        # This is here for backwards compatibility for Encryptor v2.0.0.
+        cipher.iv = options[:iv] if options[:v2_gcm_iv]
+        if options[:salt].nil?
+          # Use a non-salted cipher.
+          # This behaviour is retained for backwards compatibility. This mode
+          # is not secure and new deployments should use the :salt options
+          # wherever possible.
+          cipher.key = options[:key]
         else
-          # This is deprecated and needs to be changed.
-          cipher.pkcs5_keyivgen(options[:key])
+          # Use an explicit salt (which can be persisted into a database on a
+          # per-column basis, for example). This is the preferred (and more
+          # secure) mode of operation.
+          cipher.key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(options[:key], options[:salt], options[:hmac_iterations], cipher.key_len)
         end
-        yield cipher, options if block_given?
-        value = options[:value]
-        if cipher.authenticated?
-          if encryption?(cipher_method)
-            cipher.auth_data = options[:auth_data]
-          else
-            value = extract_cipher_text(options[:value])
-            cipher.auth_tag = extract_auth_tag(options[:value])
-            # auth_data must be set after auth_tag has been set when decrypting
-            # See http://ruby-doc.org/stdlib-2.0.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-auth_data-3D
-            cipher.auth_data = options[:auth_data]
-          end
+        cipher.iv = options[:iv] unless options[:v2_gcm_iv]
+      else
+        # This is deprecated and needs to be changed.
+        cipher.pkcs5_keyivgen(options[:key])
+      end
+
+      yield cipher, options if block_given?
+
+      value = options[:value]
+
+      if cipher.authenticated?
+        if encryption?(cipher_method)
+          cipher.auth_data = options[:auth_data]
+        else
+          value = extract_cipher_text(options[:value])
+          cipher.auth_tag = extract_auth_tag(options[:value])
+          # auth_data must be set after auth_tag has been set when decrypting
+          # See http://ruby-doc.org/stdlib-2.0.0/libdoc/openssl/rdoc/OpenSSL/Cipher.html#method-i-auth_data-3D
+          cipher.auth_data = options[:auth_data]
         end
-        result = cipher.update(value)
-        result << cipher.final
-        result << cipher.auth_tag if cipher.authenticated? && encryption?(cipher_method)
-        result
       end
 
-      def encryption?(cipher_method)
-        cipher_method == :encrypt
-      end
+      result = cipher.update(value)
+      result << cipher.final
+      result << cipher.auth_tag if cipher.authenticated? && encryption?(cipher_method)
+      result
+    end
 
-      def extract_cipher_text(value)
-        value[0..-17]
-      end
+    def encryption?(cipher_method)
+      cipher_method == :encrypt
+    end
 
-      def extract_auth_tag(value)
-        value[-16..-1]
-      end
+    def extract_cipher_text(value)
+      value[0..-17]
+    end
+
+    def extract_auth_tag(value)
+      value[-16..-1]
+    end
   end
 end
